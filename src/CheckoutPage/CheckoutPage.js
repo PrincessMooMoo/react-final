b9a3b7cb41c29807d5e3086b890d329fcc6ac123
Wendy <chriscowendy@gmail.com>
2021-02-18 17:12:21 -0800

    I don't remember. Probably more JSX

--- src/CheckoutPage/CheckoutPage.js
+++ src/CheckoutPage/CheckoutPage.js
@@ -2,6 +2,18 @@ const CheckoutPage = () => {
     return (
         <div>
             <p>This is the Checkout Page</p>
+            <form>
+                <input type="text" placeholder="Full Name"></input>
+                <br></br>
+                <input type="text" placeholder="Shipping Address"></input>
+                <br></br>
+                <input type="text" placeholder="Billing Address"></input>
+                <br></br>
+                <input type="text" placeholder="Credit Card Number"></input>
+                <input type="text" placeholder="Exp Date"></input>
+                <br></br>
+                <input type="submit"></input>
+            </form>
         </div>
     );
 }
